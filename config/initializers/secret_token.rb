11c84db6b05790c172eaa5cdaed58c6ef1ed4c8c
Maxime Eglem <maxime.eglem@gmail.com>
2012-02-27 13:42:54 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoundClone::Application.config.secret_token = '832348d6f507754f0f1dabede9566f33a741a93b253773da18476338d8e6746c021eb99711d9ca015d6caf9c95ade6bd35c5dd350ae784b48bec35bbf4ebb29d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SoundClone::Application.config.session_store :cookie_store, key: '_SoundClone_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SoundClone::Application.config.session_store :active_record_store
