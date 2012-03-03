5ae20269d12af246e9a13b5e4af0344f6580ab3b
Maxxi <max@MacBook-Pro-de-Maxime.local>
2012-03-03 20:19:44 +0000

    DRY « Don’t Repeat Yourself »
    Add title variable on static page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
   def home
+   @titre = "Home"
   end
 
   def contact
+   @titre = "Contact"
   end
   
   def about
+   @titre = "About"
   end
 
 end
