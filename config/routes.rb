4629a568033800047b85dae043a55f317680b8ba
Maxxi <max@MacBook-Pro-de-Maxime.local>
2012-03-03 19:55:42 +0000

    Add a new page "about" in the controller Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 SoundClone::Application.routes.draw do
   get "pages/home"
-
   get "pages/contact"
-
+  get "pages/about"
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
