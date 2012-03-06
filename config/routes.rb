4b5f49a5db385e80a0a6a256cb4ff4cd5f3de91e
Maxxi <max@MacBook-Pro-de-Maxime.local>
2012-03-06 12:50:07 +0000

    .DS_Store banished!

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ SoundClone::Application.routes.draw do
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
-  match '/home',    :to => 'pages#home'
   
   root :to => 'pages#home'
   
