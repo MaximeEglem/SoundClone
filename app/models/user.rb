89b745712421e481266fdb0d55b1314e90964d80
Maxxi <max@MacBook-Pro-de-Maxime.local>
2012-03-14 20:05:52 +0000

    User Model with password and profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,9 @@
 #
 
 class User < ActiveRecord::Base 
-  attr_accessible :name, :email
-
+  attr_accessor :password
+  attr_accessible :nom, :email, :password, :password_confirmation
+  
 #	/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i	full regex
 #	/	start of regex
 #	\A	match start of a string
@@ -26,6 +27,41 @@ class User < ActiveRecord::Base
 
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  validates :name,  presence: true, length: { maximum: 50 }
+#  validates :name,  presence: true, length: { maximum: 50 }
   
+   # Automatically create the virtual attribute 'password_confirmation'.
+  validates :password, :presence     => true,
+                       :confirmation => true,
+                       :length       => { :within => 6..40 }
+
+  before_save :encrypt_password
+
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil  if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+
+  private
+
+	 def encrypt_password
+      self.salt = make_salt if new_record?
+      self.encrypted_password = encrypt(password)
+    end
+
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
+
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
 end
