8f8c9c6aa961689d6f1f0598edf8d59f6821bb3c
Maxxi <max@MacBook-Pro-de-Maxime.local>
2012-03-28 23:06:59 +0100

    Created a setting page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ def create
       render 'new'
     else
       sign_in user
-      redirect_to user
+      redirect_back_or user
     end
 end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
-    def show
+before_filter :authenticate, :only => [:index, :edit, :update]
+before_filter :correct_user, :only => [:edit, :update]
+
+  def index
+    @titre = "All Users"
+    @users = User.all
+  end
+
+  def show
     @user = User.find(params[:id])
-	@title = @user.name
+    @titre = @user.name
   end
 
   def new
@@ -20,5 +28,35 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+    @title = "Edit user"
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+  
+  private
+
+    def authenticate
+	unless signed_in?
+        store_location
+        redirect_to signin_path, notice: "Please sign in."
+      end
+    end
+	
+	def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
   
 end
