278ae45f3276d0a12a87bf1a64f51b93007e9243
Maxxi <max@MacBook-Pro-de-Maxime.local>
2012-03-30 00:35:42 +0100

    Added user microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
-  before_filter :authenticate, :only => [:create, :destroy]
+ before_filter :authenticate, :only => [:create, :destroy]
+  before_filter :authorized_user, :only => :destroy
   
   def create
     @micropost = current_user.microposts.build(params[:micropost])
@@ -13,5 +14,15 @@ class MicropostsController < ApplicationController
   end
   
   def destroy
+    @micropost.destroy
+    redirect_back_or root_path
   end
+  
+   private
+
+    def authorized_user
+      @micropost = Micropost.find(params[:id])
+      redirect_to root_path unless current_user?(@micropost.user)
+    end
+	
 end
